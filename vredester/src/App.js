2793eba374579d9f24aae3f6a72aa19d580e3f76
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-01-28 07:05:05 +0100

    pop

--- vredester/src/App.js
+++ vredester/src/App.js
@@ -1,9 +1,11 @@
+import HomePage from "./pages/HomePage";
 
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-4xl font-semibold">Vredester INC</h1>
+      <HomePage/>
     </div>
   );
 }
