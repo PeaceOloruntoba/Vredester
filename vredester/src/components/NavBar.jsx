0e062b2bb50bf92eb94d5f0ae288c31b3904853b
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-08 06:17:29 +0100

    Starting it

--- vredester/src/components/NavBar.jsx
+++ vredester/src/components/NavBar.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div>
-      
+      <div className='grid text-xl font-bold'>
+        <div className="flex text-sm px-12 py-2 items-center justify-between">
+          <span>This is the header...</span>
+          <span>Social Icons ...</span>
+        </div>
+    <div className='flex items-center justify-around px-6 py-2'>
+      <p>Logo</p>
+      <ul className='flex gap-8'>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Services</a></li>
+        <li><a href="/">Portfolio</a></li>
+        <li><a href="/">Contact</a></li>
+      </ul>
+      <p>Theme toggle</p>
+      </div>
     </div>
   )
 }
