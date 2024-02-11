776b3a7d043582e73392e9e80a2004be26fc2de6
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-11 14:55:59 +0100

    a few edits

--- vredester/src/components/NavBar.jsx
+++ vredester/src/components/NavBar.jsx
@@ -7,22 +7,24 @@ import { SlScreenDesktop } from "react-icons/sl";
 export default function NavBar() {
   const options = [
     {
-      icon: "TiWeatherSunny",
+      icon: <TiWeatherSunny />,
       text: "light",
     },
     {
-      icon: "IoMoon",
+      icon: <IoMoon />,
       text: "dark",
     },
     {
-      icon: "SlScreenDesktop",
+      icon: <SlScreenDesktop />,
       text: "system",
     },
   ]
   return (
-      <div className='grid text-xl font-bold dark:bg-slate-800 bg-slate-100'>
-    <div className='flex items-center justify-around px-6 py-2'>
+      <div className='flex p-2 items-center justify-around text-xl font-bold dark:bg-slate-800 bg-slate-100'>
+      <div>
       <img src={logo} alt="" className='rounded-lg w-16' />
+      </div>
+      <div>
       <ul className='flex gap-8'>
         <li><a href="/">Home</a></li>
         <li><a href="/">About</a></li>
@@ -30,11 +32,11 @@ export default function NavBar() {
         <li><a href="/">Portfolio</a></li>
         <li><a href="/">Contact</a></li>
       </ul>
-      <div className="fixed top-5 right-10 duration-100 dark:bg-gray-950 bg-gray-100 rounded items-center justify-center dark:text-gray-100">
-        {options?.map((opt) => (
-          <button key={opt.text}>{opt.icon}</button>
-        ))}
       </div>
+      <div className="dark:bg-gray-950 bg-gray-100 rounded items-center justify-center dark:text-gray-100 p-2">
+        {options?.map((opt) => (
+          <button key={opt.text} className='rounded-full w-7 h-7 p-1 bg-slate-200 items-center justify-center text-center text-slate-800 m-1'>{opt.icon}</button>
+        ))}
       </div>
     </div>
   )
