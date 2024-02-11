572583a7794692af2986050c8dd8009fa755d796
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-11 14:35:06 +0100

    nbvcbn

--- vredester/src/components/NavBar.jsx
+++ vredester/src/components/NavBar.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import logo from "../assets/images/1.png"
+import { TiWeatherSunny } from "react-icons/ti"
+import { IoMoon } from "react-icons/io5"
+import { SlScreenDesktop } from "react-icons/sl";
+
 export default function NavBar() {
+  const options = [
+    {
+      icon: "TiWeatherSunny",
+      text: "light",
+    },
+    {
+      icon: "IoMoon",
+      text: "dark",
+    },
+    {
+      icon: "SlScreenDesktop",
+      text: "system",
+    },
+  ]
   return (
-      <div className='grid text-xl font-bold'>
-        <div className="flex text-sm px-12 py-2 items-center justify-between">
-          <span>This is the header...</span>
-          <span>Social Icons ...</span>
-        </div>
+      <div className='grid text-xl font-bold dark:bg-slate-800 bg-slate-100'>
     <div className='flex items-center justify-around px-6 py-2'>
       <img src={logo} alt="" className='rounded-lg w-16' />
       <ul className='flex gap-8'>
@@ -16,7 +30,11 @@ export default function NavBar() {
         <li><a href="/">Portfolio</a></li>
         <li><a href="/">Contact</a></li>
       </ul>
-      <p>Theme toggle</p>
+      <div className="fixed top-5 right-10 duration-100 dark:bg-gray-950 bg-gray-100 rounded items-center justify-center dark:text-gray-100">
+        {options?.map((opt) => (
+          <button key={opt.text}>{opt.icon}</button>
+        ))}
+      </div>
       </div>
     </div>
   )
