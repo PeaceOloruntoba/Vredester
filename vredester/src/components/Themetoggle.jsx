db135a99cf7bbcfc994134399983eae35ec115c4
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-12 15:46:55 +0100

    cgvbnm

--- vredester/src/components/Themetoggle.jsx
+++ vredester/src/components/Themetoggle.jsx
@@ -1,6 +1,44 @@
 import React from 'react'
 
 export default function Themetoggle() {
+  const [theme, setTheme] = useState("system");
+  const element = document.documentElement
+  const darkQuery = window.matchMedia("(prefers-color-scheme: dark)")
+  const options = [
+    {
+      icon: <TiWeatherSunny />,
+      text: "light",
+    },
+    {
+      icon: <IoMoon />,
+      text: "dark",
+    },
+    {
+      icon: <SlScreenDesktop />,
+      text: "system",
+    },
+  ];
+  function onWindowMatch() {
+    if(localStorage.theme === 'dark' || (!("theme" in localStorage) && darkQuery.matches)){
+      element.classList.add("dark")
+    } else {
+      element.classList.remove("dark")
+    }
+  }
+  useEffect(()=>{
+    switch(theme) {
+      case "dark":
+        element.classList.add('dark')
+        localStorage.setItem("theme", "dark");
+        break;
+      case "light":
+        element.classList.remove('dark')
+        localStorage.setItem("theme", "light");
+      default:
+        localStorage.setItem("theme");
+        break;
+    }
+  }, [theme])
   return (
     <div>Themetoggle</div>
   )
