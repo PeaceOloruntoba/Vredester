0eaf6f942c0a466ca2b0a35d93d9fd2c94304f50
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-22 22:03:25 +0100

    nbvcvbnm

--- vredester/src/App.js
+++ vredester/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import { logo } from './assets';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <section>
+        <nav className='flex items-center justify-around'>
+          <div>
+          <img src={logo} alt="" className='w-28'/>
+          </div>
+          <div>
+          <ul className='flex gap-10 text-2xl font-bold uppercase'>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+          </div>
+          <div>
+            <button>Theme Toggle</button>
+          </div>
+        </nav>
+      </section>
 
       <Footer />
     </div>
--- vredester/src/components/Navbar.jsx
+++ vredester/src/components/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { logo } from '../assets'
 
 export default function Navbar() {
   return (
-    <div>
+    <div className='flex items-center justify-between mr-56'>
       <div className="logo">
-        
+        <img src={logo} alt="" className='w-32'/>
+      </div>
+      <div className="">
+        <ul className='flex gap-10 text-2xl font-bold uppercase'>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+      <div className="hidden">
+        <button>Switch Toggle</button>
       </div>
     </div>
   )
