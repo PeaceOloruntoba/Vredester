f790447df19a34cae5402fd27c4001dd2e570500
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-23 09:59:42 +0100

    move

--- vredester/src/App.js
+++ vredester/src/App.js
@@ -1,26 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import { logo } from './assets';
 import Footer from './components/Footer';
 
 function App() {
+  // const [darkMode, setDarkMode] = useState(false);
+  // const toggleDarkMode = () => {
+  //   setDarkMode(!darkMode);
+  // }
   return (
-    <div className="App">
+    // <div className={`${darkMode && "dark"} App`}>
+    <div>
       <section>
-        <nav className='flex items-center justify-around'>
+        <nav className='flex items-center justify-around bg-gray-400 dark:bg-slate-900'>
           <div>
           <img src={logo} alt="" className='w-28'/>
           </div>
           <div>
           <ul className='flex gap-10 text-2xl font-bold uppercase'>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
+          <li><a href='/'>Home</a></li>
+          <li><a href='/'>About</a></li>
+          <li><a href='/'>Services</a></li>
+          <li><a href='/'>Portfolio</a></li>
+          <li><a href='/'>Contact</a></li>
         </ul>
           </div>
           <div>
-            <button>Theme Toggle</button>
+            {/* <button className='absolute rounded-full w-16 h-16 top-6 right-16 bg-slate-900 dark:bg-slate-300 text-white dark:text-black font-semibold' onClick={toggleDarkMode}>{darkMode? "LHT" : "DRK"}</button> */}
           </div>
         </nav>
       </section>
--- vredester/src/components/Footer.jsx
+++ vredester/src/components/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>This is the Footer.</div>
+    <div>This is the Footer.
+      <div className="bg-red-600 dark:bg-slate-900">
+        I am amazing
+      </div>
+    </div>
   )
 }
