3d1a0d3c788983a5263d71b92df0bff218662006
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-02-24 10:27:11 +0100

    amazing

--- vredester/src/App.js
+++ vredester/src/App.js
@@ -1,9 +1,49 @@
 // import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { logo } from './assets';
 import Footer from './components/Footer';
+import { IoSunnyOutline, IoMoon, IoDesktopOutline } from "react-icons/io5";
 
 function App() {
+  const [theme, setTheme] = useState("system")
+  const element = document.documentElement
+  const darkQuery = window.matchMedia("(prefers-color-scheme: dark)")
+  console.log(darkQuery,"darkQuery")
+  const options = [
+    {
+      icon: <IoSunnyOutline />, 
+      text: "light"
+    },
+    {
+      icon: <IoMoon />, 
+      text: "dark"
+    },
+    {
+      icon: <IoDesktopOutline />, 
+      text: "system"
+    }
+  ]
+  function onWindowMatch(){
+    // if(localStorage.theme === 'dark' || ){
+
+    // }
+  }
+  useEffect(()=>{
+    switch (theme) {
+      case 'dark':
+        element.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+        break;
+      case 'light':
+        element.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+        break;
+      default:
+        localStorage.removeItem("theme");
+        break;
+    }
+  },[theme])
   // const [darkMode, setDarkMode] = useState(false);
   // const toggleDarkMode = () => {
   //   setDarkMode(!darkMode);
@@ -17,7 +57,7 @@ function App() {
           <img src={logo} alt="" className='w-28'/>
           </div>
           <div>
-          <ul className='flex gap-10 text-2xl font-bold uppercase'>
+          <ul className='flex gap-10 text-xl font-bold uppercase'>
           <li><a href='/'>Home</a></li>
           <li><a href='/'>About</a></li>
           <li><a href='/'>Services</a></li>
@@ -25,8 +65,16 @@ function App() {
           <li><a href='/'>Contact</a></li>
         </ul>
           </div>
-          <div>
+          <div className='dark:bg-slate-800 bg-gray-100 rounded'>
             {/* <button className='absolute rounded-full w-16 h-16 top-6 right-16 bg-slate-900 dark:bg-slate-300 text-white dark:text-black font-semibold' onClick={toggleDarkMode}>{darkMode? "LHT" : "DRK"}</button> */}
+            {
+              options?.map(opt=>(
+            <button key={opt.text} onClick={()=>setTheme(opt.text)} className={`text-2xl rounded-full m-2 ${theme === opt.text && "text-sky-600"}`}>
+               {opt.icon}
+            </button>
+
+              ))
+            }
           </div>
         </nav>
       </section>
