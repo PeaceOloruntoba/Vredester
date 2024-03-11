5e74fbea9bf3cbdd6c73be722a25ef6ca9e33038
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-11 07:46:50 +0100

    Update Jumbotron.jsx

--- vredester/src/components/Jumbotron.jsx
+++ vredester/src/components/Jumbotron.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import bgimg from '../assets/images/jumbotronbg.jpg'
+import diss from '../assets/images/jumbotronimg.png'
 
 export default function Jumbotron() {
   return (
-    <div className='flex gap-8 bg-opacity-20 bg- dark:bg-[#060922]'>
-      <img src={bgimg} alt="" classname='absolute ' />
+    <div className='relative'>
+  <img src={bgimg} alt="" className='absolute inset-0 object-cover w-full h-full' />
+  <div className='inset-0 bg-white dark:bg-[#060922] opacity-80'>
+  <div className="flex justify-center items-center p-32">
+    <div className="flex flex-col">
+      <span>Social Icons</span>
+      <span>it solutions</span>
+      <span>Providing The Best Services & IT <span>Solutions</span></span>
+      <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat facilis vero delectus, quis a deserunt excepturi voluptatum qui fugiat natus aliquam ipsa,</span>
     </div>
+    <img src={diss} alt="" />
+  </div>
+</div>
+  </div>
+
   )
 }
