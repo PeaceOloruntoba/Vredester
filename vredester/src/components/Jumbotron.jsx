ab1811dcb90285572ceaa56156544a4d5c7b4f47
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-12 14:07:20 +0100

    Update Jumbotron.jsx

--- vredester/src/components/Jumbotron.jsx
+++ vredester/src/components/Jumbotron.jsx
@@ -7,14 +7,16 @@ export default function Jumbotron() {
     <div className='relative'>
   <img src={bgimg} alt="" className='absolute inset-0 object-cover w-full h-full' />
   <div className='inset-0 bg-white dark:bg-[#060922] opacity-80'>
-  <div className="flex justify-center items-center p-32">
+  <div className="grid grid-cols-2 justify-center items-center p-32">
     <div className="flex flex-col">
       <span>Social Icons</span>
-      <span>it solutions</span>
-      <span>Providing The Best Services & IT <span>Solutions</span></span>
-      <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat facilis vero delectus, quis a deserunt excepturi voluptatum qui fugiat natus aliquam ipsa,</span>
+      <span className='text-3xl text-blue-600 font-bold uppercase'>it solutions</span>
+      <span className='text-5xl font-semibold text-[#060922] dark:text-white'>Providing The Best Services & IT <span className='text-blue-600'>Solutions</span></span>
+      <span className='text-[#06092] dark:text-white text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat facilis vero delectus, quis a deserunt excepturi voluptatum qui fugiat natus aliquam ipsa,</span>
     </div>
-    <img src={diss} alt="" />
+    <div className="">
+    <img src={diss} alt="" className='' />
+    </div>
   </div>
 </div>
   </div>
