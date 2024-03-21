34fffedcb53d64c2fe02fe93588faea675cf8aa9
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-21 11:22:07 +0100

    fghj

--- vredester/src/Pages/components/Homepage/Jumbotron.jsx
+++ vredester/src/Pages/components/Homepage/Jumbotron.jsx
@@ -11,15 +11,15 @@ export default function Jumbotron() {
   <div className="grid lg:grid-cols-2 justify-center items-center p-32">
     <div className="flex flex-col gap-8">
       <span className='flex gap-8 text-[#060922] dark:text-white text-xl px-3'>
-        <a href="/" className='hover:text-blue-600'><IoLogoFacebook/></a>
-        <a href="/" className='hover:text-blue-600'><IoLogoYoutube/></a>
-        <a href="/" className='hover:text-blue-600'><IoLogoInstagram/></a>
-        <a href="/" className='hover:text-blue-600'><IoLogoTwitter/></a>
+        <a href="/" className='hover:text-[#09aff4]'><IoLogoFacebook/></a>
+        <a href="/" className='hover:text-[#09aff4]'><IoLogoYoutube/></a>
+        <a href="/" className='hover:text-[#09aff4]'><IoLogoInstagram/></a>
+        <a href="/" className='hover:text-[#09aff4]'><IoLogoTwitter/></a>
       </span>
-      <span className='text-2xl text-blue-600 font-bold uppercase'>-- it solutions  --</span>
-      <span className='text-5xl font-semibold text-[#060922] dark:text-white'>Providing The Best Services & IT <span className='text-blue-600 text-pretty underline underline-offset-8'>Solutions</span></span>
+      <span className='text-2xl text-[#09aff4] font-bold uppercase'>-- it solutions  --</span>
+      <span className='text-5xl font-semibold text-[#060922] dark:text-white'>Providing The Best Services & IT <span className='text-[#09aff4] text-pretty underline underline-offset-8'>Solutions</span></span>
       <span className='text-gray-400  text-xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed architecto dolorum inventore totam adipisci</span>
-      <a href="/"><button className='text-white text-xl uppercase bg-blue-600 w-fit px-10 py-4 opacity-80 font-bold hover:bg-transparent hover:dark:bg-transparent hover:text-blue-600 hover:dark:text-blue-600 rounded-tr-3xl rounded-bl-3xl hover:ring-2 ring-blue-600'>Contact us</button></a>
+      <a href="/"><button className='text-white text-xl uppercase bg-[#09aff4] w-fit px-10 py-4 opacity-80 font-bold hover:bg-transparent hover:dark:bg-transparent hover:text-[#09aff4] hover:dark:text-[#09aff4] rounded-tr-3xl rounded-bl-3xl hover:ring-2 ring-[#09aff4]'>Contact us</button></a>
     </div>
     <div className="">
     <img src={diss} alt="" className='' />
--- vredester/src/Pages/components/Homepage/Services.jsx
+++ vredester/src/Pages/components/Homepage/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HiArrowSmRight } from 'react-icons/hi'
 
 export default function Services() {
   return (
@@ -6,12 +7,12 @@ export default function Services() {
       <div className="grid p-32">
         <div className="grid lg:grid-cols-2 items-center justify-center">
           <div className="grid lg:pr-44 gap-4">
-        <span className='text-blue-600 text-2xl uppercase font-semibold'>--Services--</span>
+        <span className='text-[#09aff4] text-2xl uppercase font-semibold'>--Services--</span>
         <span className='capitalize font-bold text-5xl'>Services We Offer</span>
         <span className='text-gray-400 font-semibold'>Lorem Ipsum Dolor Sit Amet Consectetur Adipisicing Elit OmnisId Atque Dignissimos Repellat Quae Ullam.</span>
           </div>
           <div className="button flex flex-end justify-end">
-            <a href="/"><button className='text-white text-xl uppercase bg-blue-600 w-fit px-10 py-4 opacity-80 font-bold hover:bg-transparent hover:ring-2 ring-blue-600 hover:dark:bg-transparent hover:text-blue-600 hover:dark:text-blue-600 rounded-tr-3xl rounded-bl-3xl'>See all services</button></a>
+            <a href="/"><button className='text-white text-xl uppercase bg-[#09aff4] w-fit px-10 py-4 opacity-80 font-bold hover:bg-transparent hover:ring-2 ring-[#09aff4] hover:dark:bg-transparent hover:text-[#09aff4] hover:dark:text-[#09aff4] rounded-tr-3xl rounded-bl-3xl flex items-center justify-between gap-2 hover:gap-4'>See all services <HiArrowSmRight /></button></a>
           </div>
         </div>
       </div>
