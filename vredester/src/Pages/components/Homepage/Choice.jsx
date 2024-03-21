1bdbf6a92f649ecffa7c21446c43f8b2e4868c31
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-21 15:30:33 +0100

    Update Choice.jsx

--- vredester/src/Pages/components/Homepage/Choice.jsx
+++ vredester/src/Pages/components/Homepage/Choice.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function Choice() {
   return (
-    <div>
-      
+    <div className='bg-white dark:bg-[#060922] text-[#060922] dark:text-white lg:p-32 p-8 py-12'>
+      <div className="grid lg:flex lg:flex-row-reverse"></div>
+      <img src="" alt="" />
+      <div className="flex flex-col">
+        <span>--Why Choose Us--</span>
+        <span>Why our customers choose <span>Working</span> with us</span>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. A officia molestiae dolorum tempora ut accusamus cupiditate! Nesciunt tempora reiciendis libero voluptate!</span>
+        <span className='flex'>
+          <span>01.</span>
+          <div className="grid">
+          <span>Latest Technology</span>
+          <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque repellendus minima reiciendis nobis dolore obcaecati.</span>
+          </div>
+        </span>
+      </div>
     </div>
   )
 }
