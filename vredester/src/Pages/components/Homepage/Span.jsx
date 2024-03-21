db53769839aba1df26998cca8753a7f73c764c5f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-21 16:32:13 +0100

    Update Span.jsx

--- vredester/src/Pages/components/Homepage/Span.jsx
+++ vredester/src/Pages/components/Homepage/Span.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import pattern from '../../../assets/images/patternbg.jpg'
 
 export default function Span() {
   return (
-    <div className='bg-white dark:bg-[#060922] text-[#060922] dark:text-white lg:p-32 p-8 py-12'>
-      
+    <div className='relative text-[#060922] dark:text-white lg:p-32 lg:py-24 p-8 py-12'>
+      <div className="absolute">
+        <img src={pattern} alt="" />
+      </div>
+      <div className="flex">
+
+      </div>
     </div>
   )
 }
