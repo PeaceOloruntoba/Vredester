a0a4b0ac909552d3f1881ec9f1036619fafdfc54
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-21 16:30:02 +0100

    Update Span.jsx

--- vredester/src/Pages/components/Homepage/Span.jsx
+++ vredester/src/Pages/components/Homepage/Span.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Span() {
   return (
-    <div>Span</div>
+    <div className='bg-white dark:bg-[#060922] text-[#060922] dark:text-white lg:p-32 p-8 py-12'>
+      
+    </div>
   )
 }
