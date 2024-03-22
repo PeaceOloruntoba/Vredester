35176e417f8e0909fb524a23e530c350e7a8a91f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-22 15:16:49 +0100

    Update Services.jsx

--- vredester/src/components/Services.jsx
+++ vredester/src/components/Services.jsx
@@ -37,7 +37,6 @@ export default function Services() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
                 </svg>
-
               </span>
               <span className="dark:text-white font-bold text-2xl">Mobile Development</span>
               <span className="text-gray-300 font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque repellendus minima reiciendis nobis doloe obcarecati.</span>
