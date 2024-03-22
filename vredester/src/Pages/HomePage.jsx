8e0da8b55e61e1f8042e4477241ad96ee9126885
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-22 15:17:45 +0100

    Update HomePage.jsx

--- vredester/src/Pages/HomePage.jsx
+++ vredester/src/Pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Jumbotron from './components/Homepage/Jumbotron'
-import Services from './components/Homepage/Services'
 import About from './components/Homepage/About'
 import Choice from './components/Homepage/Choice'
 import Span from './components/Homepage/Span'
