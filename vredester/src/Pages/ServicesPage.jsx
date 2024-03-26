dacf23361bd1201c0b1de91fc90fa98f67833271
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-03-26 12:46:55 +0100

    Update ServicesPage.jsx

--- vredester/src/Pages/ServicesPage.jsx
+++ vredester/src/Pages/ServicesPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Services/Header'
 
 export default function ServicesPage() {
   return (
     <div>
-      
+      <Header />
     </div>
   )
 }
